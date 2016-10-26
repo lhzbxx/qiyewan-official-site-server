d65d9d0ab0355f6a7becf88186e246028762aa49
lhzbxx <lhzbxx@gmail.com>
2016-10-26 10:39:38 +0800

    Fix.

--- src/main/java/com/qiyewan/controller/TestController.java
+++ src/main/java/com/qiyewan/controller/TestController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
-public class AuthController {
+public class TestController {
 
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
