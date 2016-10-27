0e957ce44cd016b3350308176ea4a4280a358723
lhzbxx <lhzbxx@gmail.com>
2016-10-27 13:29:27 +0800

    Add api and exceptions package.

---
+++ src/main/java/com/qiyewan/aop/AuthAop.java
@@ -0,0 +1,41 @@
+package com.qiyewan.aop;
+
+import com.qiyewan.exceptions.NoAuthException;
+import com.qiyewan.service.TokenService;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by lhzbxx on 2016/10/27.
+ *
+ * 令牌处理
+ */
+
+@Aspect
+@Component
+public class AuthAop {
+
+    @Autowired
+    private HttpServletRequest request;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Pointcut("execution(public * com.qiyewan.api..*.*(..))")
+    public void apiAuth() {}
+
+    @Before("apiAuth()")
+    public void beforeApiAuth(JoinPoint joinPoint) throws Throwable {
+        String authorization = request.getHeader("Authorization");
+        Long userId = tokenService.getUserIdWithToken(authorization);
+        if (userId == null) throw new NoAuthException("Token无效或已过期。");
+        request.setAttribute("userId", userId);
+    }
+
+}
