ccceef921ff298492ee052dab6969a5c93a17cdd
lhzbxx <lhzbxx@gmail.com>
2016-11-01 17:27:06 +0800

    Alipay.

--- src/main/java/com/alipay/config/AlipayConfig.java
+++ src/main/java/com/alipay/config/AlipayConfig.java
@@ -16,28 +16,30 @@ public class AlipayConfig {
 //↓↓↓↓↓↓↓↓↓↓请在这里配置您的基本信息↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
 
 	// 合作身份者ID，签约账号，以2088开头由16位纯数字组成的字符串，查看地址：https://b.alipay.com/order/pidAndKey.htm
-	public static String partner = "";
+	public static String partner = "2088021990129702";
 	
 	// 收款支付宝账号，以2088开头由16位纯数字组成的字符串，一般情况下收款账号就是签约账号
-	public static String seller_id = partner;
+	public static String seller_id = "2088021990129702";
 
 	//商户的私钥,需要PKCS8格式，RSA公私钥生成：https://doc.open.alipay.com/doc2/detail.htm?spm=a219a.7629140.0.0.nBDxfy&treeId=58&articleId=103242&docType=1
-	public static String private_key = "";
+	public static String private_key = "MIICeAIBADANBgkqhkiG9w0BAQEFAASCAmIwggJeAgEAAoGBAKmyH2cBcbyejCrfzXtzT85CFQ0U9nNkGsR49A81wAS1i13VmhQ2RwmUhfpBlGEor75qGW/SatXokPDrXEYyv/oZDfwd0gEY+HhWbgNCshriR8uAjYFlpCkk6qfeKKcNrA8VQoHcV/5J7ZSwHcadpfZ1nzeajlRiMFt1N6B+NlfFAgMBAAECgYEAqJxh81lOB5qJr3gcldMnssuaWxs/HbyTlDCSYCKKHqO8TIoWxIqB3xUCYnyS0E87ADov+BndKZohmiBh2SZH9LxqFH4BRwhdffPK5eH15QHK7Pi/DBHlUPUN1m0x3mUUN+Lb1yJXCgbw9fHPfru3A5jicKgdOU2fDwTTYHQ3FykCQQDVmo0yI7k4hy8qJW7ty5EpvUxC2U/BxT/RjcFNhW4r0/19YrQ8rmnvV8ip+w1xpgNvXqw/ozUukSbI5R9++W/3AkEAy2CPMkvQDykK5U/YhWHsN64otLyIcKfmpqAOvTOxL1P48Ira7TQEBvI+FZqKRDO4PmAlaem5gLi7tu9v9dP/IwJAbLkkEVdZmcpFWYCAPA2Xr9UokgYmsIyD9VfgAmNeNnTK3B13fEOSrD7UZqDgsO49ZVyc9C9QjIlZYnwtUeYI9QJBAI1ztWUYEbfZpfhkFoZ+2xrHCQzb7FHela/itx2lLGFsmEJFcYt1mC90oPXJrM1O46COorcNlrYOxpCv7EEgl90CQQCEfB6PWFaA698Bju1wcqdWOqrjKd1uB4H81S9xRtqubTxHboZuUCCR9go6OGhdFEZoO8xUpyaCE+GKjLEC17O5";
 	
 	// 支付宝的公钥,查看地址：https://b.alipay.com/order/pidAndKey.htm
 	public static String alipay_public_key  = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCnxj/9qwVfgoUh/y2W89L6BkRAFljhNhgPdyPuBV64bfQNN1PjbCzkIM6qRdKBoLPXmKKMiFYnkd6rAoprih3/PrQEB/VsW8OoM8fxn67UDYuyBTqA23MML9q1+ilIZwBC2AQ2UBVOrFXfFl75p6/B5KsiNG9zpgmLCUYuLkxpLQIDAQAB";
 
 	// 服务器异步通知页面路径  需http://格式的完整路径，不能加?id=123这类自定义参数，必须外网可以正常访问
-	public static String notify_url = "http://商户网址/create_direct_pay_by_user-JAVA-UTF-8/notify_url.jsp";
+	// TODO: 2016/11/1 需要填写！ 
+	public static String notify_url = "";
 
 	// 页面跳转同步通知页面路径 需http://格式的完整路径，不能加?id=123这类自定义参数，必须外网可以正常访问
-	public static String return_url = "http://商户网址/create_direct_pay_by_user-JAVA-UTF-8/return_url.jsp";
+	// TODO: 2016/11/1 需要填写！
+	public static String return_url = "";
 
 	// 签名方式
 	public static String sign_type = "RSA";
 	
 	// 调试用，创建TXT日志文件夹路径，见AlipayCore.java类中的logResult(String sWord)打印方法。
-	public static String log_path = "C:\\";
+	public static String log_path = "/Users/lhzbxx/Develop/QiYeWan/New/offical-website/Server/storage/data/alipay/";
 		
 	// 字符编码格式 目前支持 gbk 或 utf-8
 	public static String input_charset = "utf-8";
