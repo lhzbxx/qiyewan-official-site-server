3b1336b3050421a71443571b11a61c201128ae5a
lhzbxx <lhzbxx@gmail.com>
2017-01-13 16:00:42 +0800

    Fix products.csv.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,14 @@
-LOAD DATA LOCAL INFILE 'classpath:/data/products.vbsv'
+LOAD DATA LOCAL INFILE 'classpath:/data/products.csv'
 INTO TABLE product
-FIELDS TERMINATED BY '|'
+FIELDS TERMINATED BY ','
+OPTIONALLY ENCLOSED BY '"'
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
-(classification_code, classification_name, comment, cover, create_at, heat, info, @var1, @var2, name, process,
- product_state, purchase_number, rate, region_code, serial_id, summary, unit, unit_price, update_at, what_need, what_obtain,
- t_what_need, t_what_obtain)
-SET is_hot = (@var1 = 'TRUE'), is_instant = (@var2 = 'TRUE');
+(classification_code, classification_name, comment, cover, create_at, heat, @var1, @var2, name, process,
+ product_state, purchase_number, rate, region_code, serial_id, summary, t_what_need, t_what_obtain,
+ unit, unit_price, update_at, what_need, what_obtain, version)
+SET is_hot = (@var1 = '1'), is_instant = (@var2 = '1');
 
 LOAD DATA LOCAL INFILE 'classpath:/data/faq.csv'
 INTO TABLE faq
@@ -63,3 +64,14 @@ IGNORE 1 LINES
 # 更新产品的评论个数：
 UPDATE product SET purchase_number = (SELECT count(product_serial_id) FROM review T WHERE T.product_serial_id = product.serial_id GROUP BY product_serial_id);
 UPDATE product SET purchase_number = 0 WHERE purchase_number IS NULL;
+
+# 更新图片规则：
+UPDATE product SET cover = CONCAT('product-', SUBSTR(serial_id, 4), '-cover.jpg'),
+ what_need = CONCAT('product-', SUBSTR(serial_id, 4), '-what-need.png'),
+ what_obtain = CONCAT('product-', SUBSTR(serial_id, 4), '-what-obtain.png');
+
+# 更新购物车中的外键。
+# UPDATE cart SET product_serial_id = serial_id;
+
+# 更新周期性产品的单位。
+UPDATE product SET unit = '月' WHERE is_instant = 0;
